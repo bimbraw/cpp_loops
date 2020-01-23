53c1089618dea4fdc410493077f7cc0d3de99e73
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2020-01-23 15:42:43 -0500

    Updated

---
+++ cpp_loops_5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; 
+    
+    cout << "Input the integer number: ";
+    cin >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "*";
+    }
+    
+    return 0;
+}
