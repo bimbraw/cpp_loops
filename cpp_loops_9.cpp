a35b9cd012a275b108242f713f0c20f8f0d9ab6d
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2020-01-24 11:41:55 -0500

    Updated

---
+++ cpp_loops_9.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+   cout << a[0][0] << "    " << a[0][1] << "    " << a[0][2] << endl << endl << endl;
+   cout << a[1][0] << "    " << a[1][1] << "    " << a[1][2] << endl << endl << endl;
+   cout << a[2][0] << "    " << a[2][1] << "    " << a[2][2] << endl << endl << endl;
+}
