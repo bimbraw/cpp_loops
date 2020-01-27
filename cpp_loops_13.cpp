ef3519469dce60443e17f237173e52321eed4848
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2020-01-27 16:51:32 -0500

    Incorrect implementation

--- cpp_loops_13.cpp
+++ cpp_loops_13.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int number, number_1, a_1;
+    int number, number_1, sum = 0;
     
     cout << "Enter an integer number: ";
     cin >> number;
@@ -15,24 +15,43 @@ int main()
     {
         number_1 = number/2;
         int a_1[number_1] = {};
+        
+        a_1[0] = 0;
+        
+        
+        for(int i = 0; i < number+1; i++)
+        {
+            a[i-1] = i;
+            
+            if(a[i-1] % 2 == 0)
+            {
+                a_1[i-1] = a_1[i-1] + a[i-1];
+                cout << a[i-1];
+            }
+            else
+            {
+                continue;
+            }
+            cout << a[i-1] + a[i+1] + a[i+3] << endl;
+            
+        }
     }
     else
     {
         number_1 = number/2 + 1;
         int a_1[number_1] = {};
-    }
-    
-    for(int i = 1; i < number+1 ; i++)
-    {
-        a[i-1] = i;
-        cout << a[i-1];
-        if(a[i-1] % 2 == 0)
+        for(int i = 1; i < number+1 ; i++)
         {
-            a_1[i-1] = a[i-1];
-        }
-        else
-        {
-            continue;
+            a[i-1] = i;
+            cout << a[i-1];
+            if(a[i-1] % 2 == 0)
+            {
+                a_1[i-1] = a[i-1];
+            }
+            else
+            {
+                continue;
+            }
         }
     }
     return 0;
