6f2a9a62f7fd6d34a724f923b4badde5a7d51c2c
Keshav Bimbraw <bimbrawkeshav@gmail.com>
2020-03-03 16:55:05 -0500

    Added first condition, but second one still remaining

--- cpp_loops_12.cpp
+++ cpp_loops_12.cpp
@@ -5,110 +5,74 @@ using namespace std;
 int main()
 {
    char a[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
-   cout << (char)a[0][0] << "    " << (char)a[0][1] << "    " << (char)a[0][2] << endl << endl << endl;
-   cout << (char)a[1][0] << "    " << (char)a[1][1] << "    " << (char)a[1][2] << endl << endl << endl;
-   cout << (char)a[2][0] << "    " << (char)a[2][1] << "    " << (char)a[2][2] << endl << endl << endl;
-   
-   int x, y;
-   
+   cout << (char)a[0][0] << "  " << (char)a[0][1] << "  " << (char)a[0][2] << endl ;
+   cout << (char)a[1][0] << "  " << (char)a[1][1] << "  " << (char)a[1][2] << endl ;
+   cout << (char)a[2][0] << "  " << (char)a[2][1] << "  " << (char)a[2][2] << endl << endl;
+
+   int x, y, p = 0;
+
    for (int i = 0; i < 5; i ++)
    {
        cout << "Input x: ";
        cin >> x;
-       if(x <= 0 && x > 3)
+       
+       if(x < 1 || x > 3)
        {
-           cout << "Wrong! Enter again. Input x: ";
-           cin >> x;
-           continue;
-       }
-       else
-       {
-           continue;
+           cout << "Incorrect x! Out of range! Bye!" << endl;
+           break;
        }
        
        cout << "Input y: ";
        cin >> y;
-       if(y <= 0 && y > 3)
-       {
-           cout << "Wrong! Enter again. Input y: ";
-           cin >> y;
-           continue;
-       }
-       else
-       {
-           continue;
-       }
        
-       
-       /*
-       if(x <= 0 && x > 3)
+       if(y < 1 || y > 3)
        {
-           cout << "Wrong! Enter again. Input x: ";
-           cin >> x;
+           cout << "Incorrect y! Out of range! Bye!" << endl;
+           break;
        }
-       
-       if(y <= 0 && y > 3)
-       {
-           cout << "Wrong! Enter again. Input y: ";
-           cin >> y;
-       }
-       */
-   
+
        system("clear");
-   
+
        a[x-1][y-1] =  'X';
+
+       cout << (char)a[0][0] << "  " << (char)a[0][1] << "  " << (char)a[0][2] << endl;
+       cout << (char)a[1][0] << "  " << (char)a[1][1] << "  " << (char)a[1][2] << endl;
+       cout << (char)a[2][0] << "  " << (char)a[2][1] << "  " << (char)a[2][2] << endl << endl;
        
-       cout << (char)a[0][0] << "    " << (char)a[0][1] << "    " << (char)a[0][2] << endl << endl << endl;
-       cout << (char)a[1][0] << "    " << (char)a[1][1] << "    " << (char)a[1][2] << endl << endl << endl;
-       cout << (char)a[2][0] << "    " << (char)a[2][1] << "    " << (char)a[2][2] << endl << endl << endl;
+       p = p + 1;
        
+       if(p > 8)
+       {
+           break;
+       }
+
        cout << "Input x: ";
        cin >> x;
-       if(x <= 0 && x > 3)
-       {
-           cout << "Wrong! Enter again. Input x: ";
-           cin >> x;
-           continue;
-       }
-       else
-       {
-           continue;
-       }
        
+       if(x < 1 || x > 3)
+       {
+           cout << "Incorrect x! Out of range! Bye!" << endl;
+           break;
+       }
        
        cout << "Input y: ";
        cin >> y;
-       if(y <= 0 && y > 3)
+
+       if(y < 1 || y > 3)
        {
-           cout << "Wrong! Enter again. Input y: ";
-           cin >> y;
-           continue;
-       }
-       else
-       {
-           continue;
+           cout << "Incorrect y! Out of range! Bye!" << endl;
+           break;
        }
        
-       
-       /*
-       if(x <= 0 && x > 3)
-       {
-           cout << "Wrong! Enter again. Input x: ";
-           cin >> x;
-       }
-       
-       if(y <= 0 && y > 3)
-       {
-           cout << "Wrong! Enter again. Input y: ";
-           cin >> y;
-       }
-       */
        system("clear");
-       
+
        a[x-1][y-1] =  'O';
        
-       cout << (char)a[0][0] << "    " << (char)a[0][1] << "    " << (char)a[0][2] << endl << endl << endl;
-       cout << (char)a[1][0] << "    " << (char)a[1][1] << "    " << (char)a[1][2] << endl << endl << endl;
-       cout << (char)a[2][0] << "    " << (char)a[2][1] << "    " << (char)a[2][2] << endl << endl << endl;
+       cout << (char)a[0][0] << "  " << (char)a[0][1] << "  " << (char)a[0][2] << endl;
+       cout << (char)a[1][0] << "  " << (char)a[1][1] << "  " << (char)a[1][2] << endl;
+       cout << (char)a[2][0] << "  " << (char)a[2][1] << "  " << (char)a[2][2] << endl << endl;
+       
+       p = p + 1;
+       
    }
 }
